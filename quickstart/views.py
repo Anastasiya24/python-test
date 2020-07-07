4d570de53ca4361a00743b0a7019ad344a568d48
A.Shuhouskaya <A.Shuhouskaya@itechart-group.com>
2020-07-08 00:27:04 +0300

    Created new route with handler, added templates

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Hello world!</h1>")
+
+def template(request):
+    title = 'Country: {0}; City: {1}'.format('Belarus', 'Mogilew')
+    times = [ 1, 2, 3, 4, 5 ]
+    content = { 
+        'firstName': 'Tomas', 
+        'lastName': 'Tester', 
+        'title': title, 
+        'times': times 
+    }
+    return render(request, 'example-template.html', content)
